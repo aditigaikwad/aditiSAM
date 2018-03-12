5a5536e06e36a7cc677c7bc398d14c4e99c12933
Aditi Gaikwad <aditig@cybage.com>
2018-03-12 18:26:09 +0530

    Added files

---
+++ aditilambdafunction.py
@@ -0,0 +1,38 @@
+import boto3
+import json
+print('Loading function')
+
+def lambda_handler(event, context):
+    ssmClient = boto3.client('ssm')
+    print("Received event: " + json.dumps(event, indent=2))
+    print(event);
+    message = event['Records'][0]['Sns']['Message']
+    print("From SNS: " + message)
+    #return message
+    
+    if message == "start": 
+        COMMANDS=["sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT"]
+    
+    if message == "stop":
+        COMMANDS=["sudo iptables -I INPUT -p tcp --dport 80 -j DROP"]
+    
+    
+    ssmCommand = ssmClient.send_command(
+             Targets =[
+                 {
+                     "Key": "tag:Name",
+                     "Values":[
+                         "Demo",
+                         ]
+                 },
+             ],
+         DocumentName = 'AWS-RunShellScript',
+         TimeoutSeconds = 5000,
+         Comment = 'Start/Stop nginx',
+         
+         Parameters = {
+             "commands": COMMANDS
+         }
+       
+     )
+    print(ssmCommand)
